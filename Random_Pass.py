6c7cab8aed145f40f6189865fb755cad27e8a00c
Rostislav <116183972+Rostislav12345@users.noreply.github.com>
2023-09-19 19:28:33 +0700

    added main pyhton file

---
+++ Random_Pass.py
@@ -0,0 +1,25 @@
+import random
+def RandPass(n):
+    '''
+    n is for legth for pass
+    
+    example:
+    
+    RandPass(8) 
+    
+    output: h8l?Pd0&
+    
+    RandPass(18)
+    
+    output: PXv28u?YnxE5R@N0gG
+    '''
+    lower_case ="abcdefghijkmnopqrstuvwxyz"
+    upper_case ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers ="01234567890"
+    symbols ="@#$%&!?*"
+    use_for = lower_case + upper_case + numbers + symbols
+    legth_for_pass = n
+    password ="".join(random.sample(use_for, legth_for_pass))
+    print(password)
+    
+    
